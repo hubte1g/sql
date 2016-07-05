1adb1fee685175e9bf64239406621f9194b489c8
hubte1g <lickteig.joshua@gmail.com>
2016-07-05 16:19:03 -0500

    Create byte-sizing.sql

---
+++ byte-sizing.sql
@@ -0,0 +1,4 @@
+SELECT OCTET_LENGTH('En un lugar de La Mancha...') LENGTH_DEFAULT,
+       OCTET_LENGTH('En un lugar de La Mancha...', ASCII) LENGTH_ASCII,
+       OCTET_LENGTH('En un lugar de La Mancha...', UTF8) LENGTH_UTF8,
+       OCTET_LENGTH('En un lugar de La Mancha...', UTF16) LENGTH_UTF16
