1f4af6adf5b391b7bd11872bb88b0e0e262d603d
hubte1g <lickteig.joshua@gmail.com>
2016-08-05 11:01:21 -0500

    Update byte-sizing.sql

--- byte-sizing.sql
+++ byte-sizing.sql
@@ -2,3 +2,9 @@ SELECT OCTET_LENGTH('En un lugar de La Mancha...') LENGTH_DEFAULT,
        OCTET_LENGTH('En un lugar de La Mancha...', ASCII) LENGTH_ASCII,
        OCTET_LENGTH('En un lugar de La Mancha...', UTF8) LENGTH_UTF8,
        OCTET_LENGTH('En un lugar de La Mancha...', UTF16) LENGTH_UTF16
+       
+SELECT        
+count(*),
+length(cast(TEST_COL as varchar(50)))
+FROM DB.TBL
+group by 2
